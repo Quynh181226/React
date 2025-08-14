782300e41a51f4c4d4cf0a9edc02c68435c9e0f9
Quynh181226 <quynh2682@icloud.com>
2025-08-14 12:14:13 +0700

    thuxemduockhong

---
+++ PTIT_CNTT2_IT104_Session08/PTIT_CNTT2_IT104_Session07_Bai01.ts
@@ -0,0 +1,10 @@
+enum weekday{
+    t2='monday',
+    t3='tuesday',
+    t4='wednesday',
+    t5='thursday',
+    t6='friday',
+    t7='saturday',
+    cn='sunday'
+}
+Object.values(weekday).forEach(e=>console.log(e))
