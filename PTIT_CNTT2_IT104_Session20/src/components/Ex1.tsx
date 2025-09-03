71f1433821e50eb64f0dfb48a26e5a82dd9c0dfe
Quynh181226 <quynh2682@icloud.com>
2025-09-03 22:34:49 +0700

    React

--- PTIT_CNTT2_IT104_Session20/src/components/Ex1.tsx
+++ PTIT_CNTT2_IT104_Session20/src/components/Ex1.tsx
@@ -3,22 +3,37 @@ import {useState} from "react";
 export const Ex1=()=>{
     const [val, setVal]=useState("")
     const styles={
-        // container: {
-        //
-        // },
+        container: {
+            fontFamily: "Arial, sans-serif",
+            padding: " 5px 10px",
+            backgroundColor: "#f8f6f6",
+            width: "50%",
+            boxShadow: "0 2px 6px 2px rgba(0,0,0,0.1)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column"
+        },
         show: {
-            width: "10.5rem",
+            width: "22.2rem",
             backgroundColor: "#FFE6E6",
-            border: "#FF8282",
-            padding: "5px"
+            border: "1px solid #FF8282",
+            borderRadius: "3px",
+            padding: "5px",
+            display: "flex",
+            justifyContent: "center",
+            color: "#FF5454"
+        },
+        input1: {
+           padding: "0.25rem 6.3rem",
+            borderRadius: "3px"
         }
     }
     return(
-        // style={styles.container}
-        <>
-            <p>Check string length input</p>
-            <input type="text" placeholder="Input here" value={val} onChange={(e)=>setVal(e.target.value)}/>
-            {val.length<5 ? (<p style={styles.show}>String input longer 5 digit</p>):""}
-        </>
+        <div style={styles.container}>
+            <h4>Check string length input</h4>
+            <input type="text" placeholder="Input here" value={val} style={styles.input1} onChange={(e)=>setVal(e.target.value)}/>
+            {val.length>5 ? (<p style={styles.show}>String input longer 5 digit</p>):""}
+        </div>
     )
 }
