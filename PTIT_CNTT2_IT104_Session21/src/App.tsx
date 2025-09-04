059471cd2110c7be237c531fe247a596496f7c00
Quynh181226 <quynh2682@icloud.com>
2025-09-04 19:35:07 +0700

    React

--- PTIT_CNTT2_IT104_Session21/src/App.tsx
+++ PTIT_CNTT2_IT104_Session21/src/App.tsx
@@ -1,20 +1,20 @@
-import {Ex1} from "./components/Ex1.tsx";
-import {Ex2} from "./components/Ex2.tsx";
-import {Ex3} from "./components/Ex3.tsx";
-import {Ex4} from "./components/Ex4.tsx";
-import {Ex5} from "./components/Ex5.tsx";
-import {Ex6} from "./components/Ex6.tsx";
+// import {Ex1} from "./components/Ex1.tsx";
+// import {Ex2} from "./components/Ex2.tsx";
+// import {Ex3} from "./components/Ex3.tsx";
+// import {Ex4} from "./components/Ex4.tsx";
+// import {Ex5} from "./components/Ex5.tsx";
+// import {Ex6} from "./components/Ex6.tsx";
 import {Ex7} from "./components/Ex7.tsx";
 
 function App() {
   return (
     <>
-        <Ex1/>
-        <Ex2/>
-        <Ex3/>
-        <Ex4/>
-        <Ex5/>
-        <Ex6/>
+        {/*<Ex1/>*/}
+        {/*<Ex2/>*/}
+        {/*<Ex3/>*/}
+        {/*<Ex4/>*/}
+        {/*<Ex5/>*/}
+        {/*<Ex6/>*/}
         <Ex7/>
     </>
   )
