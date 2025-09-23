a4d16191f9bf188afe6576bd17e9290cfb2639f5
Quynh181226 <quynh2682@icloud.com>
2025-09-23 20:59:35 +0700

    React

---
+++ PTIT_CNTT2_IT104_Session34/client/src/hooks/useRedux.ts
@@ -0,0 +1,7 @@
+import type { TypedUseSelectorHook } from 'react-redux'
+import { useDispatch, useSelector, useStore } from 'react-redux'
+import type { AppDispatch, AppStore, RootState } from '../redux/store/Store.ts'
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppStore: () => AppStore = useStore
