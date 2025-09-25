9bc574094af30c495767c16f73f365a7eb94872a
Quynh181226 <quynh2682@icloud.com>
2025-09-25 07:06:33 +0700

    React

--- PTIT_CNTT2_IT104_Session35/src/App.tsx
+++ PTIT_CNTT2_IT104_Session35/src/App.tsx
@@ -3,18 +3,18 @@ import "./index.css"
 // import {Ex4} from "./components/Ex4.tsx";
 // import {Ex3} from "./components/Ex3.tsx";
 // import {Ex2} from "./components/Ex2.tsx";
-// import {Ex5} from "./components/Ex5.tsx";
+import {Ex5} from "./components/Ex5.tsx";
 // import {Ex6} from "./components/Ex6.tsx";
 import {Ex1} from "./components/Ex1.tsx";
 function App() {
     return (
         <>
             <Ex1 />
+            <Ex5 />
             <div className="Border">
                 {/*<Ex2 />*/}
                 {/*<Ex3 />*/}
                 {/*<Ex4 />*/}
-                {/*<Ex5 />*/}
                 {/*<Ex6 />*/}
             </div>
                 {/*<Ex7 />*/}
